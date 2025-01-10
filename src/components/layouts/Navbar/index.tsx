f236a8a11ab955eccb66dd36b86e9b1154940c5a
Luis Lima <luisfernando.lima2005@gmail.com>
2025-01-10 11:05:03 -0300

    feature/Change language

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+
 import { IoLanguage } from "react-icons/io5";
 
 import { TdictionaryMenu } from "@/types/dictionary";
 
-import Link from "next/link";
+export const Navbar = ({ dictionary }: { dictionary: TdictionaryMenu }) => {
+  const [language, setLanguage] = useState("en");
 
-export const Navbar = ({dictionary} : {dictionary:TdictionaryMenu}) => {
+  const pathName = usePathname();
+
+  const redirectPathName = (locale: string) => {
+    if (!pathName) return "/";
+
+    const segments = pathName.split("/");
+    segments[1] = locale;
+    return segments.join("/");
+  };
+
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) => (prevLanguage === "en" ? "pt" : "en"));
+    console.log(language)
+  };
+
   return (
     <ul className="flex items-center justify-center gap-4">
       <li>
@@ -13,7 +34,11 @@ export const Navbar = ({dictionary} : {dictionary:TdictionaryMenu}) => {
       <li>
         <Link href={"/projects"}>{dictionary.projects}</Link>
       </li>
-      <li><IoLanguage /></li>
+      <li>
+        <Link onClick={toggleLanguage} href={redirectPathName(language)}>
+          <IoLanguage className="text-xl cursor-pointer" />
+        </Link>
+      </li>
       <li>Mudar tema</li>
     </ul>
   );
