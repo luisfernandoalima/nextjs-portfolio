94f08fb704ceef27373238204e7f30ec3addc4af
Luis Lima <luisfernando.lima2005@gmail.com>
2025-01-17 18:00:28 -0300

    feature/Using Animations

--- src/components/Home/AboutMe/index.tsx
+++ src/components/Home/AboutMe/index.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import * as motion from "motion/react-client";
+import type { Variants } from "motion/react";
+
 import Image from "next/image";
 import Atropos from "atropos/react";
 
@@ -9,27 +12,70 @@ import "./AboutMe.scss";
 
 export const AboutMe = ({ dictionary }: { dictionary: IAboutMe }) => {
   return (
-    <section className="aboutMeSection p-5">
-      <div className="aboutMeDiv">
-        <Atropos shadow={false} highlight={false}>
-          <Image
-            src={developerImage}
-            alt="Developer"
-            height={400}
-            width={400}
-          />
-        </Atropos>
-        <article>
-          <h2 className="text-4xl font-semibold text-center md:text-start indent mb-3">
-            {dictionary.title}
-          </h2>
-          {dictionary.text.map((text) => (
-            <p key={text} className="text-lg md:text-xl indent-8 text-justify mb-2">
-              {text}
-            </p>
-          ))}
-        </article>
-      </div>
-    </section>
+    <motion.div
+      initial={"hidden"}
+      whileInView={"onscreen"}
+      viewport={{ amount: 0.4, once: true }}
+    >
+      <section className="aboutMeSection p-5">
+        <div className="aboutMeDiv">
+          <motion.div variants={imgVariant}>
+          <Atropos shadow={false} highlight={false}>
+            <Image
+              src={developerImage}
+              alt="Developer"
+              height={400}
+              width={400}
+            />
+          </Atropos>
+          </motion.div>
+          <motion.div variants={textVariant}>
+            <article>
+              <h2 className="text-4xl font-semibold text-center md:text-start indent mb-3">
+                {dictionary.title}
+              </h2>
+              {dictionary.text.map((text) => (
+                <p
+                  key={text}
+                  className="text-lg md:text-xl indent-8 text-justify mb-2"
+                >
+                  {text}
+                </p>
+              ))}
+            </article>
+          </motion.div>
+        </div>
+      </section>
+    </motion.div>
   );
+};
+
+const textVariant: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 50,
+  },
+
+  onscreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const imgVariant: Variants = {
+  hidden: {
+    opacity: 0,
+    x: -50,
+  },
+
+  onscreen: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
--- src/components/Home/Experience/index.tsx
+++ src/components/Home/Experience/index.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import * as motion from "motion/react-client";
+import type { Variants } from "motion/react";
+
 import { RiNextjsFill } from "react-icons/ri";
 import {
   SiTypescript,
@@ -22,57 +27,108 @@ import { IExperience } from "@/types/dictionary";
 
 import "./Experience.scss";
 
-export const Experience = ({dictionary}:{dictionary: IExperience}) => {
+export const Experience = ({ dictionary }: { dictionary: IExperience }) => {
   return (
-    <section className="experienceSection p-5 flex items-center justify-center flex-col">
-      <h2 className="text-4xl font-semibold">{dictionary.title}</h2>
-      <div className="iconDiv mt-4">
-        <span className="html">
-          <FaHtml5 />
-        </span>
-        <span className="css">
-          <FaCss3 />
-        </span>
-        <span className="scss">
-          <FaSass />
-        </span>
-        <span className="js">
-          <FaJs />
-        </span>
-        <span className="ts">
-          <SiTypescript />
-        </span>
-        <span className="react">
-          <FaReact />
-        </span>
-        <span className="next">
-          <RiNextjsFill />
-        </span>
-        <span className="bootstrap">
-          <FaBootstrap />
-        </span>
-        <span className="tailwind">
-          <SiTailwindcss />
-        </span>
-        <span className="node">
-          <FaNode />
-        </span>
-        <span className="mysql">
-          <SiMysql />
-        </span>
-        <span className="mongo">
-          <SiMongodb />
-        </span>
-        <span className="vite">
-          <SiVite />
-        </span>
-        <span className="git">
-          <FaGit />
-        </span>
-        <span className="github">
-          <FaGithub />
-        </span>
-      </div>
-    </section>
+    <motion.div
+      initial="offscreen"
+      whileInView={"onscreen"}
+      viewport={{ amount: 0.3, once: true }}
+    >
+      <section className="experienceSection p-5 flex items-center justify-center flex-col">
+        <h2 className="text-4xl font-semibold">{dictionary.title}</h2>
+        <div className="iconDiv mt-4">
+          {IconsList.map((item, index) => (
+            <motion.div
+              key={index}
+              custom={index}
+              initial="hidden"
+              whileInView="visible"
+              variants={spanVariants}
+              viewport={{ amount: 0.3, once: true }}
+            >
+              <span className={item.class}>
+              {item.icon}
+              </span>
+            </motion.div>
+          ))}
+        </div>
+      </section>
+    </motion.div>
   );
 };
+
+const spanVariants: Variants = {
+  hidden: { opacity: 0, y: 30 },
+  visible: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: i * 0.1,
+      duration: 0.5,
+      ease: "easeOut",
+    },
+  }),
+};
+
+const IconsList = [
+  {
+    class: "html",
+    icon: <FaHtml5 />,
+  },
+  {
+    class: "css",
+    icon: <FaCss3 />,
+  },
+  {
+    class: "scss",
+    icon: <FaSass />,
+  },
+  {
+    class: "js",
+    icon: <FaJs />,
+  },
+  {
+    class: "ts",
+    icon: <SiTypescript />,
+  },
+  {
+    class: "react",
+    icon: <FaReact />,
+  },
+  {
+    class: "next",
+    icon: <RiNextjsFill />,
+  },
+  {
+    class: "bootstrap",
+    icon: <FaBootstrap />,
+  },
+  {
+    class: "tailwind",
+    icon: <SiTailwindcss />,
+  },
+  {
+    class: "node",
+    icon: <FaNode />,
+  },
+  {
+    class: "mysql",
+    icon: <SiMysql />,
+  },
+  {
+    class: "mongo",
+    icon: <SiMongodb />,
+  },
+  {
+    class: "vite",
+    icon: <SiVite />,
+  },
+  {
+    class: "git",
+    icon: <FaGit />,
+  },
+  {
+    class: "github",
+    icon: <FaGithub />,
+  },
+];
