2dc163f44dfbe314871113f7591f9909fa19a694
Luis Lima <luisfernando.lima2005@gmail.com>
2025-01-23 10:38:26 -0300

    feature/Contact section

--- src/components/Home/Contact/index.tsx
+++ src/components/Home/Contact/index.tsx
@@ -1,24 +1,110 @@
-import { IContact } from '@/types/dictionary';
+"use client";
 
-import './Contact.scss';
-import { FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa';
+import * as motion from "motion/react-client";
+import type { Variants } from "motion/react";
+import Atropos from "atropos/react";
+import "atropos/css";
 
-export const Contact = ({dictionary}: {dictionary:IContact}) => {
-    return(
-        <section className='contactSection py-9 px-5'>
-            <h2 className="text-4xl font-semibold text-center">{dictionary.title}</h2>
+import Image from "next/image";
 
-            <div className="buttonArea">
-          <a href="https://github.com/luisfernandoalima" target="_blank">
-            <button className="github"><FaGithub/><span>GitHub</span></button>
-          </a>
-          <a href="mailto:lima.luisfernando.dev@gmail.com">
-            <button className="gmail"><FaGoogle/><span>E-mail</span></button>
-          </a>
-          <a href="https://www.linkedin.com/in/luis-fernando-a-43aaa2230/" target="_blank">
-            <button className="linkedin"><FaLinkedin/><span>LinkedIn</span></button>
-          </a>
+import { IContact } from "@/types/dictionary";
+
+import imgContact from "../../../../public/icons/contatos.png";
+import "./Contact.scss";
+import { FaGithub, FaGoogle, FaLinkedin } from "react-icons/fa";
+
+export const Contact = ({ dictionary }: { dictionary: IContact }) => {
+  return (
+    <motion.div
+      initial={"hidden"}
+      whileInView={"onscreen"}
+      viewport={{ amount: 0.3, once: true }}
+    >
+      <section className="contactSection py-9 px-5">
+        <h2 className="text-4xl font-semibold text-center  mb-2">
+          {dictionary.title}
+        </h2>
+        <div className="iconsDiv mt-4">
+          <div className="buttonArea">
+            {contactsLinks.map((link, index) => (
+              <motion.a
+                key={index}
+                href={link.link}
+                target="_blank"
+                variants={buttonsVariants}
+                custom={index}
+              >
+                <button className={link.class}>
+                  {link.icon}
+                  <span>{link.name}</span>
+                </button>
+              </motion.a>
+            ))}
+          </div>
+          <motion.div variants={imageVariant}>
+            <Atropos shadow={false} highlight={false}>
+              <Image
+                src={imgContact}
+                alt="Contact Icon"
+                height={300}
+                width={300}
+              />
+            </Atropos>
+          </motion.div>
         </div>
-        </section>
-    )
-}
+      </section>
+    </motion.div>
+  );
+};
+
+const imageVariant: Variants = {
+  hidden: {
+    x: 50,
+    opacity: 0,
+  },
+
+  onscreen: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const buttonsVariants: Variants = {
+  hidden: {
+    x: -50,
+    opacity: 0,
+  },
+  onscreen: (i: number) => ({
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: i * 0.2,
+      duration: 0.5,
+      ease: "easeOut",
+    },
+  }),
+};
+
+const contactsLinks = [
+  {
+    link: "https://github.com/luisfernandoalima",
+    name: "GitHub",
+    class: "github",
+    icon: <FaGithub />,
+  },
+  {
+    link: "mailto:lima.luisfernando.dev@gmail.com",
+    name: "E-mail",
+    class: "gmail",
+    icon: <FaGoogle />,
+  },
+  {
+    link: "https://www.linkedin.com/in/luis-fernando-a-43aaa2230/",
+    name: "LinkedIn",
+    class: "linkedin",
+    icon: <FaLinkedin />,
+  },
+];
