95b72bef3f18b7cc34a07f27c9752d91f4c227a0
Luis Lima <luisfernando.lima2005@gmail.com>
2025-02-14 22:30:18 -0300

    feature/Metadata config

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,30 @@
+import { Metadata } from "next";
+
 import { getDictionary } from "@/lib/get-dictionary";
 import { Locale } from "@/i18n-config";
 
 import { Header } from "@/components/layouts/Header";
 import { Footer } from "@/components/layouts/Footer";
+
+export const metadata:Metadata = {
+  title: "Home",
+  description: "Portfolio de Desenvolvedor Full-Stack JavaScript.",
+  openGraph: {
+    title: "Portfolio: Luis Fernando Alves de Lima",
+    description:"Portfolio de Desenvolvedor Full-Stack JavaScript.",
+    images: ['https://nextjs-portfolio-smoky-phi-35.vercel.app/public/OpenGraphImage.png']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
+}
+
 
 export default async function SiteLayout({
   children,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,4 @@
-import { Metadata } from "next";
-
-
 import "@/styles/globals.css";
-
-export const metadata:Metadata = {
-  title: "Home",
-  description: "Portfolio de Desenvolvedor Full-Stack JavaScript.",
-  openGraph: {
-    title: "Portfolio: Luis Fernando Alves de Lima",
-    description:"Portfolio de Desenvolvedor Full-Stack JavaScript.",
-    images: ['https://nextjs-portfolio-smoky-phi-35.vercel.app/public/OpenGraphImage.png']
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: true
-    }
-  }
-}
 
 export default async function RootLayout({
   children,
