d26d00b729c0247f21317cb64b2dd8bbee0a3058
Luis Lima <luisfernando.lima2005@gmail.com>
2025-02-14 21:19:02 -0300

    feature/Metadata config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata:Metadata = {
   title: "Luis Fernando Alves de Lima",
   description: "Portfolio de Desenvolvedor Full-Stack JavaScript.",
   openGraph: {
-    title: "Portfolio Luis Fernando Alves de Lima",
+    title: "Portfolio: Luis Fernando Alves de Lima",
     description:"Portfolio de Desenvolvedor Full-Stack JavaScript.",
     images: ['https://nextjs-portfolio-smoky-phi-35.vercel.app/public/OpenGraphImage.png']
   }
